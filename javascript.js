41c2099f4fd778c90b90e158df3e118c0355c6f6
Jurrair <m.jurrairfreelancer66@gmail.com>
2025-03-13 00:01:49 +0500

    in this i have make the logic but there is some issue with

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 let box = document.querySelectorAll(".box");
 let resetbutton = document.querySelector("#resset");
+let msg = document.querySelector("msg-winner");
+let winner = document.querySelector(".winner");
 
 let turnO = true;
 
 let winpattern = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
     [1,4,7],
     [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
 ];
 
 box.forEach((box) => {
@@ -25,6 +27,25 @@ box.forEach((box) => {
             box.innerText = "X";
             turnO = true;
         }
+        box.disabled = true;
+        checkwinner();
     });
 });
+
+const checkwinner = () => {
+    for(let pattern of winpattern) {
+        
+        let p1 =    box[pattern[0]].innerText;
+        let p2 =    box[pattern[1]].innerText;
+        let p3 =    box[pattern[2]].innerText;
+
+        if(p1 != "" && p2 != "" && p3 != "") {
+            if(p1 === p2 && p2 === p3) {
+                console.log("winner", p1);
+            }
+        }
+        
+        
+    }
+};
 
